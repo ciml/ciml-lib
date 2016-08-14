f1ca42f9d438581bf7133d2981a49a52503fa9ad
gabrieldias <gabriel199716@gmail.com>
2016-08-14 16:37:00 -0300

    representacao de arvore em vetor

--- genetica/arvlin.c
+++ genetica/arvlin.c
@@ -3,11 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-void semeadora(Arvlin *arv,int nivelMax){//ainda nao funciona...
+/*void semeadora(Arvlin *arv,int nivelMax){//ainda nao funciona...
     int i=0,aux,nivel;Pilha p;
     iniPilha(&p,nivelMax);
     for(i=0,nivel=0;i < arv->max;i++){
-        if(nivel<nivelMax){            
+        if(nivel<nivelMax){
             if(geraArv(arv,i)!=0){
                 empilha(&p,i);
                 nivel++;
@@ -39,8 +39,8 @@ void semeadora(Arvlin *arv,int nivelMax){//ainda nao funciona...
     arv->Nfilhos=i;
     descarrega(&p);
 }
-
-int geraArv(Arvlin *arv,int indice){//retorna numero de filhos e preenche a arvore No a No
+*/
+int geraArv(Arvlin *arv,int indice){//retorna numero de filhos e preenche um no ale...
     arv->filhos[indice]=rand()%3;
     arv->elementos[indice]= arv->filhos[indice] == 0 ? rand()%10 : 
         arv->filhos[indice] == 1 ? (rand()%10)+10 : (rand()%10)+20 ;
@@ -48,14 +48,14 @@ int geraArv(Arvlin *arv,int indice){//retorna numero de filhos e preenche a arvo
 }
 
 
-void iniArvore(Arvlin *arv,int N){
+void iniArvore(Arvlin *arv,int N){//gera os vetores dinamicam...
     arv->Nfilhos=0;
     arv->max=N;
     arv->elementos=malloc(sizeof(int)*N);
     arv->filhos=malloc(sizeof(int)*N);
 }
 
-void arvPrintVetor(Arvlin *arv){
+void arvPrintVetor(Arvlin *arv){//print linear do vetor
     int i=0;
     printf(" VETOR DE ELEMENTOS \n ");
     for(i=0;i<arv->Nfilhos;i++){
@@ -69,7 +69,7 @@ void arvPrintVetor(Arvlin *arv){
     printf("\n");
 }
 
-void printNivel(Arvlin *arv,int nivel,int i){
+void printNivel(Arvlin *arv,int nivel,int i){//modelo da saida do printArv...
     int k=0;
     printf("N(%2d)//I(%2d)",nivel,i);
     for(;k<nivel;k++){
@@ -78,38 +78,37 @@ void printNivel(Arvlin *arv,int nivel,int i){
     printf("%d\n",arv->elementos[i]);
 }
 
-Arvlin* arvTest(){//preenche uma arvore test pre-definida;
-    int i,Nel=14;
-    Arvlin *arv=malloc(sizeof(Arvlin));
-    //int    testeFilhos[]={2,2,2,0,0,0,2,0,0};//{2,1,1,0,2,0,2,0 ,1,2,0,0};////////{1,2,0,0};;;{2,2,0,0,2,0,0};/////
-    int testeElementos[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14};//{1,2,3,6,7,8,9,10,11,13,14,12};//{0,1,2,3,4,5,6,7,8,9,10};////{1,2,3,4};{1,2,4,5,3,6,7};//{1,2,4,5,3,6,7};//{1,2,3,6,7,8,9,10,11,13,14,12};////
-    int    testeFilhos[]={2,1,1,0,2,0,2,0 ,1,2,0,0};
-    //int    testeFilhos[]={1,1,2,2,0,2,1,0,1,0,0};
-    //int    testeFilhos[]={1,2,1,0,0};   
-    //int    testeFilhos[]={2,2,2,0,0,0,2,0,0};
-    iniArvore(arv,Nel);
-    arv->Nfilhos=Nel;
-    for(i=0;i< arv->Nfilhos;i++){
+Arvlin* arvTest(){//preenche uma arvore test pre-definida; flag 10 no fim 
+    int i;
+    Arvlin *arv=malloc(sizeof(Arvlin));    
+    int testeElementos[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+    //int    testeFilhos[]={2,1,1,0,2,0,2,0,1,2,0,0,10};
+    //int    testeFilhos[]={2,2,2,0,1,0,0,1,0,10};
+    //int    testeFilhos[]={1,1,2,2,0,2,1,0,1,0,0,10};
+    //int    testeFilhos[]={2,2,1,0,0,0,10};   
+    int    testeFilhos[]={2,2,2,0,0,0,2,0,0,10};
+    iniArvore(arv,20);
+    for(i=0;testeFilhos[i]!=10;i++){
         arv->filhos[i]=testeFilhos[i];
         arv->elementos[i]=testeElementos[i];
-    }
+    }    
+    arv->Nfilhos=i;
+    printf("\n teste:%d",i);
     return arv;
 }
 
-void printArvore(Arvlin *arv){
-    int i;
-    Pilha p;iniPilha(&p,arv->Nfilhos);
+
+void printArvore(Arvlin *arv){//print sem uso de marcaçao no 2 e vetor filhos nao e nec...
+    int i;Pilha p;iniPilha(&p,arv->Nfilhos);
     for(i=0;i<arv->Nfilhos;i++){
-        printNivel(arv,p.numero+1,i);
-        //empilha(&p,i);
+        printNivel(arv,p.numero+1,i);    
         if(arv->filhos[i]!=0){                    
             empilha(&p,arv->filhos[i]);            
-        }else
-            if(topoPilha(&p)==2){//resolver bug dos 3 zeros               
-                    p.carga[p.numero]=1;
-            }else{              
+        }else{
                 for(; p.numero!=-1 && topoPilha(&p)==1;desempilha(&p));
-            }
+                if(topoPilha(&p)==2)
+                    p.carga[p.numero]=1;            
+        }
     }
     descarrega(&p);
 }
