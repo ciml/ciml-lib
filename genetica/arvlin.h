78bd026b5a7925df5c6508a479748f1e2a2abd50
gabrieldias <gabriel199716@gmail.com>
2016-08-15 15:34:26 -0300

    representacao de arvore em vetor

--- genetica/arvlin.h
+++ genetica/arvlin.h
@@ -5,19 +5,32 @@
 extern "C" {
 #endif
 
-    typedef struct{
+     typedef struct{
       int Nfilhos;
       int *elementos;
       int *filhos;
       int max;
     }Arvlin;
     void iniArvore(Arvlin *arv,int N);
+    //aloca arvore, precisa ser modificada para memoria estatica
     void arvPrintVetor(Arvlin *arv);
+    //imprime na forma linear
     void printNivel(Arvlin *arv,int nivel,int i);
+    //formato do output do print arv
     Arvlin *arvTest();
+    //gera arvore modelo
     void printArvore(Arvlin *arv);
+    //imprime arvore na forma nivel+++elem
     void semeadora(Arvlin *arv,int nivelMax);
-    int geraArv(Arvlin *arv,int indice);    
+    //gera arvore aleatoria com nivel max
+    int geraArv(Arvlin *arv,int indice);
+    //gera os nos da semeadora
+    void mutaArv(Arvlin *arv,Arvlin *arvM);
+    //insere nova subarvore gerada
+    void skipElemArv(Arvlin *arv,int tamPulo,int onde);
+    //desloca elementos da arvore
+    int calcTamSubArv(int *filhosArv,int indice);
+    //calcula tamanho de uma subarvore usando o vetor de filhos
 
 #ifdef __cplusplus
 }
