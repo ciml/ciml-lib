0544d44224ef25955c32cc6d0b9b7ef7cde8ae69
gabrieldias <gabriel199716@gmail.com>
2016-08-20 14:36:11 -0300

    representacao de arvore em vetor

--- genetica/arvlin.c
+++ genetica/arvlin.c
@@ -2,12 +2,21 @@
 #include "pilha.h"
 #include <stdio.h>
 #include <stdlib.h>
+#define TAM_MAX_ARV 100
 
 void iniArvore(Arvlin *arv,int N){
     arv->Nfilhos=0;
     arv->max=N;
     arv->elementos=malloc(sizeof(int)*N);
     arv->filhos=malloc(sizeof(int)*N);
+}
+
+void lenhador(Arvlin *arv){
+    free(arv->elementos);
+    free(arv->filhos);
+    arv->elementos=0;
+    arv->filhos=0;
+    printf("\nÁrvore Podada\n");
 }
 
 void arvPrintVetor(Arvlin *arv){
@@ -106,25 +115,29 @@ int geraArv(Arvlin *arv,const int* indice){//retorna numero de filhos e preenche
     return arv->filhos[(*indice)];
 }
 
-void mutaArv(Arvlin *arv,Arvlin *arvM){
-    int i,tamPulo;
+void mutaArv(Arvlin *arv){
+    int i,tamPulo;Arvlin arvM;
+    iniArvore(&arvM,TAM_MAX_ARV);
+    semeadora(&arvM,(rand()%arv->Nfilhos)); 
+    printArvore(&arvM);
     int Icol=(rand()%arv->Nfilhos),tamSubArv=calcTamSubArv(arv->filhos,&Icol);//indice onde colocar
-    tamPulo = arvM->Nfilhos - tamSubArv;
+    tamPulo = arvM.Nfilhos - tamSubArv;
     printf("==== vou colocar no indice %d, pulando %d ==== \n",Icol,tamPulo);
     if(arv->max >= tamPulo + arv->Nfilhos ){//cabe?
         skipElemArv(arv,&tamPulo,&Icol,&tamSubArv);
-        copiaSubArv(arv,arvM,Icol,0);
+        copiaSubArv(arv,&arvM,Icol,0,&tamSubArv);
     }else{
         printf("impossivel realizar mutacao devido ao tamanho");
     }
+    lenhador(&arvM);
 }
 
-void copiaSubArv(Arvlin* arv,Arvlin* arv2,int onde1,int onde2){    
-    int tamSubArv2=calcTamSubArv(arv2->filhos ,&onde2);   
-    for(; onde2 < tamSubArv2 ;onde1++,onde2++){
-            arv->elementos[onde1]=arv2->elementos[onde2];//colocar ruido aqui;
-            arv->filhos[onde1]=arv2->filhos[onde2];
-        }
+void copiaSubArv(Arvlin* arv,Arvlin* arv2,int onde1, int onde2,const int* tamCopia ){    
+    int i;   
+    for(i=0; i < (*tamCopia) ;i++){        
+        arv->elementos[onde1+i]=arv2->elementos[onde2+i];//colocar ruido aqui;
+        arv->filhos[onde1+i]=arv2->filhos[onde2+i];
+    }
 }
 
 int calcTamSubArv(int *filhosArv,const int* indice){
@@ -149,4 +162,46 @@ void skipElemArv(Arvlin *arv,const int* tamPulo,const int* onde,const int* tamSu
         }        
     }
     arv->Nfilhos+=(*tamPulo);
+}
+
+void combinacao(Arvlin *arv1,Arvlin *arv2){//copia uma subarvore na outra
+    int icol1=(rand()%arv1->Nfilhos), icol2=(rand()%arv2->Nfilhos);//indices das subarvores
+    int tamSubArv1 = calcTamSubArv(arv1->filhos,&icol1);
+    int tamSubArv2 = calcTamSubArv(arv2->filhos,&icol2);
+    printf("tamsubarv 1 =%d , 2 = %d",tamSubArv1,tamSubArv2);
+    int tamPulo1= (tamSubArv2 - tamSubArv1);
+    int tamPulo2= (tamSubArv1 - tamSubArv2);
+    printf("\n====vou colocar na arv1 ind= %d skip=%d na arv2 ind= %d skip=%d ====\n",
+            icol1,tamPulo1,icol2,tamPulo2);    
+    if( (arv1->max >= (tamPulo1 + arv1->Nfilhos)) 
+            && (arv2->max >= (tamPulo2 + arv2->Nfilhos))){//cabe nos dois
+        
+        if(tamSubArv1 >= tamSubArv2) {//achar menor sub para a copia paralela
+            trocaSubArv(arv1,arv2,&icol1,&icol2,tamSubArv2);
+            skipElemArv(arv2,&tamPulo2,&icol2,&tamSubArv2);
+            if(tamSubArv1!=tamSubArv2)
+                copiaSubArv(arv2,arv1,(icol2+tamSubArv2),(icol1+tamSubArv2),&tamPulo2);
+            skipElemArv(arv1,&tamPulo1,&icol1,&tamSubArv1);
+            
+        }else{            
+            trocaSubArv(arv2,arv1,&icol2,&icol1,tamSubArv1);
+            skipElemArv(arv1,&tamPulo1,&icol1,&tamSubArv1);
+            copiaSubArv(arv1,arv2,icol1+tamSubArv1,icol2+tamSubArv1,&tamPulo1);
+            skipElemArv(arv2,&tamPulo2,&icol2,&tamSubArv2);
+            
+        }
+    }
+}
+
+void  trocaSubArv(Arvlin* arvM,Arvlin* arvm,int *icol1,int *icol2,int tamSubArvMenor){
+    int i=0,aux;
+    while(i<tamSubArvMenor){
+        aux=arvM->elementos[*icol1+i];
+        arvM->elementos[*icol1+i]=arvm->elementos[*icol2+i];
+        arvm->elementos[*icol2+i]=aux;
+        aux=arvM->filhos[*icol1+i];
+        arvM->filhos[*icol1+i]=arvm->filhos[*icol2+i];
+        arvm->filhos[*icol2+i]=aux;
+        i++;
+    }        
 }
