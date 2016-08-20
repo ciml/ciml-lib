3d0808c57554b9edd4f132fa6ac2b5f972db2ce2
gabrieldias <gabriel199716@gmail.com>
2016-08-20 14:36:18 -0300

    representacao de arvore em vetor

--- genetica/main.c
+++ genetica/main.c
@@ -2,25 +2,29 @@
 #include "arvlin.h"
 #include <stdio.h>
 int main(int argc, char** argv) {
-    srand(30);
+    srand(14);
     printf("inciando programa...\n");
     //Arvlin *arv=arvTest();
     Arvlin a,b;
     iniArvore(&a,100);
-    iniArvore(&b,10);
+    iniArvore(&b,100);
     //arvTest(&arv);
     semeadora(&a,7);
-    semeadora(&b,3);
+    semeadora(&b,7);
     printf("printando vetor...\n");
-    arvPrintVetor(&a);
-    arvPrintVetor(&b);
+    //arvPrintVetor(&a);
+    //arvPrintVetor(&b);
     printf("printando arvore...\n");
     //printArvore(arv);   
     printArvore(&a);
     printArvore(&b);
-    printf("mutaçao");
-    mutaArv(&a,&b);
-    arvPrintVetor(&a);
+    //printf("mutaçao");
+    //mutaArv(&a);
+    printf("combinando\n");
+    combinacao(&a,&b);
+    //arvPrintVetor(&a);
     printArvore(&a);
+    //arvPrintVetor(&b);
+    printArvore(&b);
     return (EXIT_SUCCESS);
 }
