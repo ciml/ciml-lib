9ee5b9f8b9d90d04e69996262f9d1f93284a1685
Bruno Henrique Rodrigues <41806574+brunohr@users.noreply.github.com>
2019-06-15 12:08:10 -0300

    Update main.cpp

--- src/tabelaVerdade/main.cpp
+++ src/tabelaVerdade/main.cpp
@@ -7,9 +7,9 @@
 
 using namespace std;
 
-vector<int> dec2bin(int num, int aux)
+vector<bool> dec2bin(int num, int aux)
 {
-    vector<int> bin(aux);
+    vector<bool> bin(6, 0);
     for(int i = 0; i < aux; i++, num /= 2)
         if(num > 0)
             bin[i] = num%2;
@@ -17,64 +17,62 @@ vector<int> dec2bin(int num, int aux)
     return bin;
 }
 
-int bin2dec(int num)
+int bin2dec(vector<bool> bin)
 {
-    int dec = 0, bin = 1;
-    while(num > 0)
-    {
-        dec += (num%10)*bin;
-        bin *= 2;
-        num /= 10;
-    }
+    int dec = 0;
+    for(int i = bin.size()-1; i >= 0; i--)
+        dec += pow(2, bin[i]);
     return dec;
 }
 
-int computaSaida(vector<int> entrada, int nEntradas, char c)
+int computaSaida(vector<bool> entrada, char c)
 {
     int numA = 0, numB = 0;
-    for(int i = 0; i < (nEntradas/2); i++)
+    for(int i = 0; i < 3; i++)
     {
         numA += entrada[i]*pow(2, 2-i);
-        numB += entrada[i+3]*pow(2, 2-i); //nEntradas/2
+        numB += entrada[i+3]*pow(2, 2-i);
     }
     return ((c == '+') ? (numA+numB) : (numA*numB));
 }
 
-vector<int> vecSaida(vector<int> entrada, int nEntradas, char c, int numBits)
+vector<bool> vecSaida(vector<bool> entrada, int nEntradas, char c, int numBits)
 {
-    int saida = computaSaida(entrada, nEntradas, c);
+    int saida = computaSaida(entrada, c);
     return dec2bin(saida, numBits);
 }
 
 void criarTabelaVerdade(int nEntradas, char tipoSaida)
 {
-    ofstream outfile("../circuito/tabela6x.txt"); //pasta do projeto principal
+    ofstream outfile("../circuito/tabela6+.txt"); //pasta do projeto principal
 
-    vector<int> entrada(nEntradas, 1);
-    int maiorSaida = computaSaida(entrada, nEntradas, tipoSaida);
-    int nLinhasTabela = pow(2, nEntradas), nSaidas = ceil(log2(maiorSaida+1));
-    vector<int> saidas(nSaidas);
+    int nLinhasTabela = pow(2, nEntradas);
+    vector<bool> auxSaida(6, 0), entrada, saida;
+    for(int i = 6-nEntradas; i < 6; i++)
+        auxSaida[i] = 1;
+    int nSaidas = computaSaida(auxSaida, tipoSaida), maxSaida = (int)log2(nSaidas)+1;
 
-    cout << "Entradas | Saidas" << endl;
-    cout << nLinhasTabela << "        " << nSaidas << endl;
-    outfile << nLinhasTabela << " " << nSaidas << endl;
+    cout << "Linhas tabela | Saidas" << endl;
+    cout << nLinhasTabela << "                    " << maxSaida << endl;
+    outfile << nLinhasTabela << " " << maxSaida << endl;
 
     for(int i = 0; i < nLinhasTabela; i++)
     {
         entrada = dec2bin(i, nEntradas);
-        saidas = vecSaida(entrada, nEntradas, tipoSaida, nSaidas);
+        saida = vecSaida(entrada, nEntradas, tipoSaida, nSaidas);
 
-        cout << "Entrada " << ((i+1 < 10) ? "0" : "") << i+1 << ": ";
-        for(int j = 0; j < nEntradas; j++)
+        for(int j = 6-nEntradas; j < 6; j++)
         {
-            cout << entrada[j] << " ";
+            cout << entrada[j] << ((j == 5) ? " " : "");
             outfile << entrada[j] << " ";
         }
-        for(int j = 0; j < nSaidas; j++)
+        for(int j = 6-maxSaida; j < 6; j++)
         {
-            cout << saidas[j] <<  ((j == nSaidas-1) ? "\n" : " ");
-            outfile << saidas[j] <<  ((j == nSaidas-1) ? "\n" : " ");
+            cout << saida[j];
+            outfile << saida[j] << (j == 5 ? "" : " ");
         }
+        cout << ((i == nLinhasTabela-1) ? "" : "\n");
+        outfile << ((i == nLinhasTabela-1) ? "" : "\n");
     }
     outfile.close();
 }
@@ -86,7 +84,7 @@ int main()
 
     cout << "Numero de entradas:" << endl;
     cin >> nEntradas;
-    cout << "Tipo de saida (+ ou *):" << endl;
+    cout << "Tipo de saida (+ ou x):" << endl;
     cin >> tipoSaida;
 
     criarTabelaVerdade(nEntradas, tipoSaida);
