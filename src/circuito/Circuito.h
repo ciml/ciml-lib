182f49814db82123039fced7d52ff99562c81eb0
Bruno Henrique Rodrigues <41806574+brunohr@users.noreply.github.com>
2019-11-26 14:09:49 -0300

    Add files via upload

--- src/circuito/Circuito.h
+++ src/circuito/Circuito.h
@@ -1,51 +1,16 @@
 #ifndef CIRCUITO_H
 #define CIRCUITO_H
-#include <algorithm>
-#include <cmath>
-#include <fstream>
-#include <math.h>
-#include <numeric>
-#include <vector>
-#include <time.h>
 
-using namespace std;
 
 class Circuito
 {
     public:
-        Circuito(int nLinhas, int nColunas, int levelsBack, int filhos); //lê o .txt e cria a tabela
-        virtual ~Circuito(){ };
-
-        bool geneAtivo(vector< vector <int> > mapa, int val);
-
-        int calcAcertos(int row);
-        int levelBackFunc(int col);
-        int funcNosAtivos(vector< vector<int> > mapa);
-        int nosAtivos(vector< vector<int> > mapa);
-        int novoValor(int pos);
-
-        vector<int> funcAlteracao(vector<int> vec, vector< vector<int> > mapa);
-        vector<int> funcVetorAleatorio(vector< vector <int> > mapa);
-
-        void auxMapear(vector< vector <int> > mapa, int pos);
-        void funcIn(vector<int> vec);
-        void funcV7();
-        void mapear(vector<int> vec, vector< vector<int> > mapa);
-
-    private:
-        int nEntradas;
-        int nLinhas;
-        int nColunas;
-        int nSaidas;
-        int nLinhasTabela;
-        int levelsBack;
-        int nFilhos;
-
-        vector< vector<bool> > Entrada;
-        vector< vector<bool> > Tabela;
+        Circuito();
+        virtual ~Circuito();
 
     protected:
 
+    private:
 };
 
 #endif // CIRCUITO_H
